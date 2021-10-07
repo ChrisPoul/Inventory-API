e006f00e389e4d603a5e0958d6b96d56ba1d71d6
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-10-06 19:39:50 -0500

    Set max lenght for product name

--- EnGo/models/product.py
+++ EnGo/models/product.py
@@ -8,7 +8,7 @@ from . import db, Model
 
 class Product(db.Model, Model):
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String(100), nullable=False, unique=True)
     price = Column(Integer, nullable=False, default=0)
     time_created = Column(DateTime, server_default=func.now())
     time_updated = Column(DateTime, server_onupdate=func.now())
