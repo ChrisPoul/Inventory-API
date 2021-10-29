3be954c1c929e5b010273cb9f50a6b511129fb2e
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-10-29 01:16:25 -0500

    Updated config file to work with mongodb

--- EnGo/config.py
+++ EnGo/config.py
@@ -1,12 +1,9 @@
 import os
 
-db_dialect = os.environ.get('DB_DIALECT', default='mysql')
-db_host = os.environ.get('DB_HOST', default='localhost')
-db_name = os.environ.get('DB_NAME', default='EnGo_db')
-db_user = os.environ.get('DB_USERNAME', default='admin')
-db_password = os.environ.get('DB_PASSWORD', default='')
-db_port = os.environ.get('DB_PORT', default='3306')
-
 MONGODB_SETTINGS = dict(
-    db="EnGo_db"
+    db=os.environ.get('DB_NAME', default='EnGo_db'),
+    host=os.environ.get('DB_HOST', default='localhost'),
+    port=int(os.environ.get('DB_PORT', default=27017)),
+    username=os.environ.get('DB_USERNAME', default=''),
+    password=os.environ.get('DB_PASSWORD', default='')
 )
